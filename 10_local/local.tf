571fac35b46c7e59f091c3f5b99fc3ab6bc6abac
Chinmay Jaiswal <chinmayjaiswalcj@gmail.com>
2023-01-13 08:05:42 +0530

    adds file resource using local_sensitive_file resource type

--- 10_local/local.tf
+++ 10_local/local.tf
@@ -4,3 +4,8 @@ resource "local_file" "hello_terraform" {
   content = "Welcome to terraform!! This file is created using terraform. Provider local and ResourceType: local_file and ResourceName: hello_terraform "
 }
 
+resource "local_sensitive_file" "sensitive_content" {
+  filename = "sensitive_content.txt"
+  content = "Welcome to terraform!! This file is created using terraform. Provider local and ResourceType: local_file and ResourceName: hello_terraform "
+}
+
