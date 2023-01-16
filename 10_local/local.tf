c4f25f4b628437958eb00557e82888faa2ab6e67
Chinmay Jaiswal <chinmayjaiswalcj@gmail.com>
2023-01-16 06:52:34 +0530

    [Variables] adds examples on types of variables and usages in terraform

--- 10_local/local.tf
+++ 10_local/local.tf
@@ -1,11 +1,17 @@
 #This terraform script creates simple file
 resource "local_file" "hello_terraform" {
-  filename = "hello.txt"
+  filename = "./target/hello.txt"
   content = "Welcome to terraform!! This file is created using terraform. Provider local and ResourceType: local_file and ResourceName: hello_terraform "
 }
 
 resource "local_sensitive_file" "sensitive_content" {
-  filename = "sensitive_content.txt"
+  filename = "./target/sensitive_content.txt"
   content = "Welcome to terraform!! This file is created using terraform. Provider local and ResourceType: local_file and ResourceName: hello_terraform "
 }
+
+resource "local_file" "jedi" {
+  filename = var.jedi["filename"]
+  content = var.jedi["content"]
+}
+
 
